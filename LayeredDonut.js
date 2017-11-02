d8fac4ba7a10f1caac88b7b75a9cf8bfb5000df6
DESKTOP-F1VAQ0V\ashle <ashley.currie@ucalgary.ca>
2017-11-02 01:08:31 -0600

    Donut levels can be selected and deselected

--- LayeredDonut.js
+++ LayeredDonut.js
@@ -2,35 +2,14 @@ var layeredDonut = function(){
 
 	var maxRadius;
 	var bandWidth;
-	var levels;
 	var seperation;
 	var path;
 	
 	function chart(selection){
 		
 		selection.each( function(data){
-			var radius = maxRadius - ((path.length - 1) * (bandWidth + seperation));
+			var radius = maxRadius;
 			createLevels(this, radius, data, 0, path);
-			
-			
-			/**for( var i = 0; i <= levels; i++){
-					
-				var levelData = data.filter(function(d){ return d.level == i;});
-								
-				d3.select(this)
-				.append("g")
-				.attr("transform" , "translate(" + (maxRadius - radius) +"," + (maxRadius - radius) +")")
-				.datum(levelData)
-				.call(donut()
-						.radius(radius)
-						.bandWidth(bandWidth)
-						.value(function(d){ return d.value;})
-						.classes("arc arc-level" + i ));				
-
-				radius-= (bandWidth + seperation);
-			}**/
-			
-			
 		});
 	}
 	function createLevel (container, radius, data, level){
@@ -47,6 +26,9 @@ var layeredDonut = function(){
 	
 	function createLevels(container, radius, root, level, path){
 		
+		if(root.length == 0){
+			return;
+		}
 		createLevel(container, radius, root, level);
 		
 		if(path.length == 0){
@@ -54,12 +36,9 @@ var layeredDonut = function(){
 		}
 		
 		var child = root.filter(function(d){ return d.name == path[0];})[0];
-		path.shift();
-		radius += (bandWidth + seperation);
+		radius -= (bandWidth + seperation);
 		
-		if(!(child.children.size <= 0)){
-			createLevels(container, radius, child.children, level + 1, path);
-		}
+		createLevels(container, radius, child.children, level + 1, path.slice(1));
 	}
 
 	chart.maxRadius = function(value){
@@ -75,14 +54,7 @@ var layeredDonut = function(){
 		bandWidth = value;
 		return this;
 	}
-	
-	chart.levels = function(value){
-		if(!arguments.length) return levels;
 		
-		levels = value;
-		return this;
-	}
-	
 	chart.seperation = function(value){
 		if(!arguments.length) return seperation;
 		
--- Main.js
+++ Main.js
@@ -1,48 +1,108 @@
-const MAX_RADIUS = 600;
+const MAX_RADIUS = 400;
 const BAND_WIDTH = 50;
 const SEPERATION = 70;
 
 window.onload = function init(){
-	
+	//TODO: tooltip, titles on chart, color selected, switch dates
 	d3.csv("data.csv", function(data){
 		
 		var selectedYear = "1974";
 		var parsedData = parseData(data);
 		
 		var selectedData = parsedData.filter(function(d){ return d.year == selectedYear; })[0];
-									
-		var donutChart = layeredDonut()
-						.maxRadius(MAX_RADIUS)
-						.bandWidth(BAND_WIDTH)
-						.seperation(SEPERATION)
-						.path(["Carcase meat", "Pork", "Pork joints"]);
-					
+						
 		d3.select("#container")
-		.attr("width", (MAX_RADIUS + BAND_WIDTH) * 2)
-		.attr("height", (MAX_RADIUS + BAND_WIDTH) * 2)
 		.datum(selectedData.data)
-		.call(donutChart);
+		.call(expandingDonut());
 			
+	});
+}
+
+
+var expandingDonut = function (){
+	var path;
+	var container;
+	var savedData;
+	
+	function chart(selection){
+		selection.each(function(data){
+			
+			container = this;
+			savedData = data;
+			path = [];
+			
+			create();
+		});
+	}
+	
+	this.clearChart = function(){
+		d3.select(container)
+		.selectAll("*")
+		.remove();
+	}
+	
+	this.createChart = function(){
+		var donutChart = layeredDonut()
+					.maxRadius(MAX_RADIUS)
+					.bandWidth(BAND_WIDTH)
+					.seperation(SEPERATION)
+					.path(path);
+					
+					
+			d3.select(container)
+			.attr("width", (MAX_RADIUS + BAND_WIDTH) * 2)
+			.attr("height", (MAX_RADIUS + BAND_WIDTH) * 2)
+			.datum(savedData)
+			.call(donutChart);	
+	}
+	
+	this.createInteraction = function(){
+		
 		d3.selectAll(".arc").on("mousedown", function(d){	
-			d["selected"] = !d["selected"];
-			
-			d3.selectAll(".arc")
-			.filter(function(d2){return d2.data.parent == d.data.name && d2.data.level == (d.data.level - 1);})
-			.classed("arc-expand", d["selected"]);
-			
-			d3.select("#container")
-			.datum()
-			.call(donutChart);
+		
+			var index = path.indexOf(d.data.name);
+			if(index != -1){ 
+				for (i = path.length - 1; i >= index; i--){
+					path.pop();
+				}
+			}
+			else if(path.length >=d.data.level){
+				for(i = path.length - 1; i >= d.data.level; i--){
+					path.pop();
+				}
+				path.push(d.data.name);
+			}
+			else{
+				path.push(d.data.name);
+			}
+			create();
+			markSelected();
 		})
 		.on("mouseover", function(){
 			d3.select(this).classed("arc-hover", true);
 		})
 		.on("mouseout", function(){
 			d3.select(this).classed("arc-hover", false);
-		});
-		
-	});
+		});	
+	}
+	
+	this.markSelected = function(){
+		d3.select(container)
+		.selectAll(".arc")
+		.filter(function(d){ return path.indexOf(d.name) != -1; })
+		.classed("arc-select", true);
+	}
+	
+	this.create = function(){
+		clearChart();
+		createChart();
+		createInteraction();
+	}
+	
+	return chart;
 }
+
+
 
 function parseData(raw){
 	
@@ -52,10 +112,8 @@ function parseData(raw){
 	dates.forEach(function(date){
 		var data = [];
 		raw.forEach(function(row){
-			//data.push({"name" : findName(levels, row), "parent" : findParent(levels, row), "level" : findLevel(levels, row), 
-			//"value" : (row[date] == '')? 0 : +row[date], "unit" : row.unit}); 
 			
-			recurse(data, levels, row, (row[date] == '')? 0 : +row[date]);
+			recurse(data, levels, row, (row[date] == '')? 0 : +row[date], 0);
 			
 		});
 		years.push({"year" : date, "data" : data}); 
@@ -67,15 +125,13 @@ function parseData(raw){
 	
 }
 
-
-
-function recurse(root, levelLabels, row, value){
+function recurse(root, levelLabels, row, value, level){
 	
 	var cat;
-	var level = levelLabels[0];
-	var existing = root.filter(function(d){ return d.name == row[level];});
+	var levelLabel = levelLabels[0];
+	var existing = root.filter(function(d){ return d.name == row[levelLabel];});
 	if(existing.length == 0){
-		cat = {"name" : row[level], "unit": row.unit, "children" : []};
+		cat = {"name" : row[levelLabel], "unit": row.unit, "children" : []};
 		root.push(cat); 
 	}
 	else{
@@ -83,36 +139,12 @@ function recurse(root, levelLabels, row, value){
 	}
 	if(levelLabels.length == 1 || row[levelLabels[1]] == ''){
 		cat["value"] = value;
+		cat["level"] = level;
 		return;
 	}
-	recurse(cat.children, levelLabels.slice(1), row, value); 
+	recurse(cat.children, levelLabels.slice(1), row, value, level + 1); 
 }
 
-function findLevel(levelTitles, row){
-	var levelTitle = levelTitles[levelTitles.length - 1];
-	if(row[levelTitle] != ''){
-		return 0;
-	}
 
-	return 1 + findLevel(levelTitles.slice(0, levelTitles.length -1), row);
-}
-
-function findName(levelTitles, row){
-	var levelTitle = levelTitles[levelTitles.length - 1];
-	if(row[levelTitle] != ''){
-		return row[levelTitle];
-	}
-
-	return findName(levelTitles.slice(0, levelTitles.length -1), row);
-}	
-
-function findParent(levelTitles, row){
-	var levelTitle = levelTitles[levelTitles.length - 1];
-	if(row[levelTitle] != ''){
-		return row[levelTitles[levelTitles.length - 2]];
-	}
-	
-	return findParent(levelTitles.slice(0, levelTitles.length -1), row);
-}
 
 
